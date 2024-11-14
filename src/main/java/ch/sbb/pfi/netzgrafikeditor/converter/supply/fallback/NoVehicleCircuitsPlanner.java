47b082bfbdc52c704676d544cce3107cbbcb7abc
u228298 <merlin.unterfinger@sbb.ch>
2024-11-14 10:25:40 +0100

    style: rename to supply impl to fallback

--- src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/supply/fallback/NoVehicleCircuitsPlanner.java
+++ src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/supply/fallback/NoVehicleCircuitsPlanner.java
@@ -1,4 +1,4 @@
-package ch.sbb.pfi.netzgrafikeditor.converter.supply.impl;
+package ch.sbb.pfi.netzgrafikeditor.converter.supply.fallback;
 
 import ch.sbb.pfi.netzgrafikeditor.converter.supply.DepartureInfo;
 import ch.sbb.pfi.netzgrafikeditor.converter.supply.RouteDirection;

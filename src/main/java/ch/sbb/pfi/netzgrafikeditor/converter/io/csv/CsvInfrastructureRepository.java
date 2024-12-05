5b628542f4650250a0a298bc17c1180d58aed062
u228298 <merlin.unterfinger@sbb.ch>
2024-12-05 15:56:37 +0100

    reformat: extract duplicated CSV reader code into abstract base class

--- src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/io/csv/CsvInfrastructureRepository.java
+++ src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/io/csv/CsvInfrastructureRepository.java
@@ -6,71 +6,33 @@ import ch.sbb.pfi.netzgrafikeditor.converter.core.supply.TrackSegmentInfo;
 import ch.sbb.pfi.netzgrafikeditor.converter.core.supply.TransitRouteInfo;
 import ch.sbb.pfi.netzgrafikeditor.converter.util.spatial.Coordinate;
 import ch.sbb.pfi.netzgrafikeditor.converter.util.spatial.HaversineDistance;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.io.ByteOrderMark;
-import org.apache.commons.io.input.BOMInputStream;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Slf4j
-public class CsvInfrastructureRepository implements InfrastructureRepository {
-
-    private final Map<String, StopFacilityInfo> stopFacilityInfos = new HashMap<>();
+public class CsvInfrastructureRepository extends CsvRepository<StopFacilityInfo> implements InfrastructureRepository {
 
     public CsvInfrastructureRepository(Path filePath) throws IOException {
-        log.info("Reading stop facility info CSV file: {}", filePath);
+        super(filePath, record -> {
+            String stopId = record.get("stop_id");
+            String stopName = record.get("stop_name");
+            double stopLat = Double.parseDouble(record.get("stop_lat"));
+            double stopLon = Double.parseDouble(record.get("stop_lon"));
 
-        try (FileInputStream fileInputStream = new FileInputStream(
-                filePath.toFile()); BOMInputStream bomInputStream = BOMInputStream.builder()
-                .setInputStream(fileInputStream)
-                .setByteOrderMarks(ByteOrderMark.UTF_8)
-                .get(); InputStreamReader reader = new InputStreamReader(bomInputStream, StandardCharsets.UTF_8)) {
-
-            CSVFormat format = CSVFormat.DEFAULT.builder().setHeader().setIgnoreHeaderCase(true).setTrim(true).build();
-
-            try (CSVParser csvParser = new CSVParser(reader, format)) {
-                csvParser.forEach(record -> {
-                    String stopId = record.get("stop_id");
-                    String stopName = record.get("stop_name");
-                    double stopLat = Double.parseDouble(record.get("stop_lat"));
-                    double stopLon = Double.parseDouble(record.get("stop_lon"));
-
-                    stopFacilityInfos.put(stopId,
-                            new StopFacilityInfo(stopId, stopName, new Coordinate(stopLat, stopLon)));
-                });
-            }
-        }
+            return new Entry<>(stopId, new StopFacilityInfo(stopId, stopName, new Coordinate(stopLat, stopLon)));
+        });
     }
 
     @Override
     public StopFacilityInfo getStopFacility(String stopId, String stopName, double x, double y) {
-        // ignore stop name, x and y screen coordinate from network graphic
-
-        return getStopFacility(stopId);
-    }
-
-    private StopFacilityInfo getStopFacility(String stopId) {
-        StopFacilityInfo stopFacilityInfo = stopFacilityInfos.get(stopId);
-        if (stopFacilityInfo == null) {
-            throw new IllegalArgumentException("No stop facility entry not found in CSV for " + stopId);
-        }
-
-        return stopFacilityInfo;
+        return getEntity(stopId);
     }
 
     @Override
     public List<TrackSegmentInfo> getTrack(StopFacilityInfo fromStop, StopFacilityInfo toStop, TransitRouteInfo transitRouteInfo) {
-        Coordinate fromCoord = getStopFacility(fromStop.getId()).getCoordinate();
-        Coordinate toCoord = getStopFacility(toStop.getId()).getCoordinate();
+        Coordinate fromCoord = getEntity(fromStop.getId()).getCoordinate();
+        Coordinate toCoord = getEntity(toStop.getId()).getCoordinate();
 
         return List.of(
                 new TrackSegmentInfo(String.format("%s-%s", fromStop.getId(), toStop.getId()), fromCoord, toCoord,
--- src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/io/csv/CsvRollingStockRepository.java
+++ src/main/java/ch/sbb/pfi/netzgrafikeditor/converter/io/csv/CsvRollingStockRepository.java
@@ -3,67 +3,32 @@ package ch.sbb.pfi.netzgrafikeditor.converter.io.csv;
 import ch.sbb.pfi.netzgrafikeditor.converter.core.supply.RollingStockRepository;
 import ch.sbb.pfi.netzgrafikeditor.converter.core.supply.VehicleTypeInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.io.ByteOrderMark;
-import org.apache.commons.io.input.BOMInputStream;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
-public class CsvRollingStockRepository implements RollingStockRepository {
+public class CsvRollingStockRepository extends CsvRepository<VehicleTypeInfo> implements RollingStockRepository {
 
     private static final double KMH_TO_MS = 3.6;
 
-    private final Map<String, VehicleTypeInfo> vehicleTypeInfos = new HashMap<>();
-
     public CsvRollingStockRepository(Path filePath) throws IOException {
-        log.info("Reading vehicle type info CSV file: {}", filePath);
+        super(filePath, record -> {
+            String category = record.get("category");
+            String vehicleTypeId = record.get("vehicle_type_id");
+            int seats = Integer.parseInt(record.get("seats"));
+            int standingRoom = Integer.parseInt(record.get("standing_room"));
+            double length = Double.parseDouble(record.get("length"));
+            double maxVelocity = Double.parseDouble(record.get("max_velocity")) / KMH_TO_MS;
 
-        try (FileInputStream fileInputStream = new FileInputStream(
-                filePath.toFile()); BOMInputStream bomInputStream = BOMInputStream.builder()
-                .setInputStream(fileInputStream)
-                .setByteOrderMarks(ByteOrderMark.UTF_8)
-                .get(); InputStreamReader reader = new InputStreamReader(bomInputStream, StandardCharsets.UTF_8)) {
-
-            CSVFormat format = CSVFormat.DEFAULT.builder().setHeader().setIgnoreHeaderCase(true).setTrim(true).build();
-
-            try (CSVParser csvParser = new CSVParser(reader, format)) {
-                csvParser.forEach(record -> {
-
-                    String category = record.get("category");
-                    String vehicleTypeId = record.get("vehicle_type_id");
-                    int seats = Integer.parseInt(record.get("seats"));
-                    int standingRoom = Integer.parseInt(record.get("standing_room"));
-                    double length = Double.parseDouble(record.get("length"));
-                    double maxVelocity = Double.parseDouble(record.get("max_velocity")) / KMH_TO_MS;
-
-                    vehicleTypeInfos.put(category,
-                            new VehicleTypeInfo(vehicleTypeId, seats, standingRoom, length, maxVelocity,
-                                    new HashMap<>()));
-                });
-            }
-        }
-    }
-
-    private VehicleTypeInfo getVehicleTypeInfo(String category) {
-        VehicleTypeInfo vehicleTypeInfo = vehicleTypeInfos.get(category);
-        if (vehicleTypeInfo == null) {
-            throw new IllegalArgumentException("No vehicle type entry not found in CSV for " + category);
-        }
-
-        return vehicleTypeInfo;
+            return new Entry<>(category,
+                    new VehicleTypeInfo(vehicleTypeId, seats, standingRoom, length, maxVelocity, new HashMap<>()));
+        });
     }
 
     @Override
     public VehicleTypeInfo getVehicleType(String category) {
-        return getVehicleTypeInfo(category);
+        return getEntity(category);
     }
 }
-
